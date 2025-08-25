1da0ef8121a91c3e113e0089375a3d598b862026
DSE <shahbaz.akhter9823@gmail.com>
2025-08-25 19:18:57 +0530

    Update server.js

--- ingestion-api/server.js
+++ ingestion-api/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app.js';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Ingestion API listening on port ${PORT}`);
 });
